d82781ba01c0493c7f1a440bd5be765ddb1b4471
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-02-26 10:08:32 +0530

    Add project files.

---
+++ BankApplication/Customer_Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class Customer_Account
+    {
+        /// <summary>
+        /// Fields
+        /// </summary>
+        string bank_name;
+        long customer_accountNo;
+        string customer_name;
+
+        /// <summary>
+        /// Initializes a new instance of the Customer_Account class with the specified account number and customer name.
+        /// </summary>
+        /// <param name="accountNo">The customer's account number.</param>
+        /// <param name="customerName">The customer's name.</param>
+        public Customer_Account(long accountNo, string customerName)
+        {
+            this.customer_accountNo = accountNo;
+            this.customer_name = customerName;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the bank associated with the account.
+        /// </summary>
+        public string BankName
+        {
+            get { return bank_name; }
+            set { bank_name = value; }
+        }
+
+        /// <summary>
+        /// Prints the bank details including bank name, account number, and customer name to the console.
+        /// </summary>
+        public void printInfo()
+        {
+            Console.WriteLine("Bank Name: " + bank_name);
+            Console.WriteLine("Account Number: " + customer_accountNo);
+            Console.WriteLine("Customer Name: " + customer_name);
+        }
+    }
+}
---
+++ BankApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer_Account customer = new Customer_Account(123456789, "GAUTAM LATHIYA");
+            customer.BankName = "STATE BANK OF INDIA";
+
+            // Display the information of the customer
+            customer.printInfo();
+        }
+    }
+}
